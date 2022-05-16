bd590548c38d1a86fe37643f9231c1eaf1c2bbf6
Domenico Giambra <zorinik@gmail.com>
2022-05-16 23:27:49 +0200

    First attempt at config migrations

--- src/Config.php
+++ src/Config.php
@@ -16,12 +16,9 @@ class Config
 	public static function loadEnv(): void
 	{
 		if (!self::$envLoaded) {
-			if (!defined('INCLUDE_PATH'))
-				throw new \Exception('Please define "INCLUDE_PATH" constant');
-
-			if (file_exists(INCLUDE_PATH . '.env')) {
+			if (file_exists('../../../../.env')) {
 				$dotenv = new Dotenv();
-				$dotenv->load(INCLUDE_PATH . '.env');
+				$dotenv->load('../../../../.env');
 			}
 
 			self::$envLoaded = true;
@@ -43,30 +40,29 @@ class Config
 	 * Get specified config; if not present, gets the default
 	 *
 	 * @param string $key
-	 * @param callable $default - Must return default config as an array
-	 * @param callable|null $migrateFunction - Migration function from ModEl v3 - Takes the file path as a string and returns a new string
+	 * @param array $migrations
 	 * @return array
 	 * @throws \Exception
 	 */
-	public static function get(string $key, callable $default, ?callable $migrateFunction = null): array
+	public static function get(string $key, array $migrations = []): array
 	{
 		// If it's the first time for this run I'm requesting this config
 		if (!isset(self::$internalCache[$key])) {
 			if (!in_array($key, ['redis', 'cache']) and InstalledVersions::isInstalled('model/cache') and InstalledVersions::isInstalled('model/redis') and \Model\Redis\Redis::isEnabled()) {
 				// If there is a redis caching library installed, I use it to retrieve it (or store it) from there
-				// I do not cache config for "redis" and "cache" libraries, or it would results in an infinite recursion
+				// I do not cache config for "redis" and "cache" libraries, or it would result in an infinite recursion
 
 				$cache = \Model\Cache\Cache::getCacheAdapter('redis');
-				self::$internalCache[$key] = $cache->get('model.config.' . $key, function (\Symfony\Contracts\Cache\ItemInterface $item) use ($key, $default, $migrateFunction) {
-					$item->expiresAfter(300);
+				self::$internalCache[$key] = $cache->get('model.config.' . $key, function (\Symfony\Contracts\Cache\ItemInterface $item) use ($key, $migrations) {
+					$item->expiresAfter(3600 * 24);
 					$item->tag('config');
-					return self::retrieveConfig($key, $default, $migrateFunction);
+					return self::retrieveConfig($key, $migrations);
 				});
 
 				\Model\Cache\Cache::registerInvalidation('tag', ['config'], 'redis');
 			} else {
 				// Otherwise I just retrieve it from file
-				self::$internalCache[$key] = self::retrieveConfig($key, $default, $migrateFunction);
+				self::$internalCache[$key] = self::retrieveConfig($key, $migrations);
 			}
 		}
 
@@ -80,15 +76,14 @@ class Config
 	}
 
 	/**
-	 * Internal method for retrieves config directly without cache
+	 * Internal method for retrieving config directly without cache
 	 *
 	 * @param string $key
-	 * @param callable $default
-	 * @param callable|null $migrateFunction
+	 * @param array $migrations
 	 * @return array
 	 * @throws \Exception
 	 */
-	private static function retrieveConfig(string $key, callable $default, ?callable $migrateFunction = null): array
+	private static function retrieveConfig(string $key, array $migrations = []): array
 	{
 		$configPath = self::getConfigPath();
 
@@ -99,18 +94,51 @@ class Config
 
 		$filepath = $configPath . DIRECTORY_SEPARATOR . $key . '.php';
 
-		if (!file_exists($filepath)) {
-			if (!self::migrateOldConfig($key, $migrateFunction)) {
-				$default = call_user_func($default);
-				if (!is_array($default))
-					throw new \Exception('Default config must be an array');
+		if (file_exists($filepath)) {
+			$config = require($filepath);
 
-				if (!file_put_contents($filepath, "<?php\nreturn " . var_export(['production' => $default], true) . ";\n"))
-					throw new \Exception('Error while writing config file');
+			if (!isset($config['meta'])) {
+				$config['meta'] = [
+					'version' => null,
+				];
 			}
+		} else {
+			$config = [
+				'meta' => [
+					'version' => null,
+				],
+				'production' => [],
+			];
 		}
 
-		return require($filepath);
+		$latestVersion = null;
+		foreach ($migrations as $migration) {
+			if (!is_array($migration) or !isset($migration['version'], $migration['migration']))
+				throw new \Exception('Wrong config migration format');
+
+			if (version_compare($config['meta']['version'] ?? '0.0.0', $migration['version']) >= 0)
+				continue;
+
+			foreach ($config as $configEnv => $configReal) {
+				if ($configEnv === 'meta')
+					continue;
+
+				$config[$configEnv] = call_user_func($migration['migration'], $configReal, $configEnv);
+				if (!is_array($config[$configEnv]))
+					throw new \Exception('Config must be an array');
+			}
+
+			$latestVersion = $migration['version'];
+		}
+
+		if ($config['meta']['version'] !== $latestVersion) {
+			$config['meta']['version'] = $latestVersion;
+
+			if (!file_put_contents($filepath, "<?php\nreturn " . var_export($config, true) . ";\n"))
+				throw new \Exception('Error while writing config file');
+		}
+
+		return $config;
 	}
 
 	/**
@@ -120,43 +148,6 @@ class Config
 	 */
 	private static function getConfigPath(): string
 	{
-		return INCLUDE_PATH . ($_ENV['CONFIG_PATH'] ?? 'config');
-	}
-
-	/**
-	 * Migration utility from ModEl v3 config files
-	 *
-	 * @param string $key
-	 * @param callable|null $migrateFunction
-	 * @return bool
-	 */
-	private static function migrateOldConfig(string $key, ?callable $migrateFunction = null): bool
-	{
-		if ($migrateFunction === null) {
-			$migrateFunction = function (string $configPath) {
-				require($configPath);
-				if (isset($config)) {
-					$config = [
-						'production' => $config,
-					];
-
-					return "<?php\nreturn " . var_export($config, true) . ";\n";
-				} else {
-					return null;
-				}
-			};
-		}
-
-		$configPath = self::getConfigPath();
-		$oldKey = str_replace(' ', '', ucwords(str_replace('-', ' ', $key)));
-		if (file_exists($configPath . DIRECTORY_SEPARATOR . $oldKey . DIRECTORY_SEPARATOR . 'config.php')) {
-			$migratedConfig = $migrateFunction($configPath . DIRECTORY_SEPARATOR . $oldKey . DIRECTORY_SEPARATOR . 'config.php');
-			if ($migratedConfig !== null)
-				return (bool)file_put_contents($configPath . DIRECTORY_SEPARATOR . $key . '.php', $migratedConfig);
-
-			return false;
-		}
-
-		return false;
+		return '../../../../' . ($_ENV['CONFIG_PATH'] ?? 'config');
 	}
 }
